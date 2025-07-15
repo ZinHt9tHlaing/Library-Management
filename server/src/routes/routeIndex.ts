68c174937722eec7510312c057289194cf68de8b
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-07-15 11:01:12 +0630

    Library Management server startup

---
+++ server/src/routes/routeIndex.ts
@@ -0,0 +1,11 @@
+import { Application, Router } from "express";
+import { ProductRouter } from "./productRoute";
+
+const _routes: Array<[string, Router]> = [["/product", ProductRouter]];
+
+export const routes = (app: Application) => {
+  _routes.forEach((route) => {
+    const [url, router] = route;
+    app.use(url, router);
+  });
+};
